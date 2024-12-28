f939fc6f70ff0a662c58ed4504793046ef9acd32
Diamond Wilson <diamondsrrare@gmail.com>
2024-12-28 07:14:13 +0700

    arrays

---
+++ array.js
@@ -0,0 +1,17 @@
+//let newArray = [element1, element2, element3];
+//console.log(newArray[index number]);
+//array is a special type of object
+//arrays hold multiple values and data types called elements
+//they are ordered and accessed using index position
+//the first index is zero
+//stores multiple related items
+//let numbers = new Array(element1, element2, element3);
+//console.log(numbers);
+//unlike objects, no key-value pairs so no dot.notation
+/*
+Operations:
+-iteration
+-sort
+-filter
+-transform
+*/
